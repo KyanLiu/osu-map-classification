ec5aefb5bca6cde98e0f71435108e97a8d7dffbc
kyan <tbnrthunder9@gmail.com>
2025-05-17 17:15:16 -0400

    read data, and link together map data and map classifications

---
+++ assets/dataset/mapType.py
@@ -0,0 +1,13 @@
+
+# Map conversion Array:
+# [Speed, Aim, Tech, Flow Aim, Stream, Burst, Consistency]
+mapConversion = ["speed", "jump aim", "tech", "flow aim", "stream", "burst", "consistency"]
+
+mapClasses = {
+    "ericsaade_popular_celebrity.osu": [0, 1, 0, 0, 0, 0, 0],
+    "frederic_oddloop_oldnoob.osu": [0, 0, 0, 0, 0, 0, 0],
+    "linkinpark_breakingthehabit_turbokolab.osu": [0, 1, 0, 0, 0, 0, 0],
+    "thelivingtombstone_goodbyemoonmen_cyb3rsomniverse.osu": [0, 1, 0, 0, 0, 0, 0],
+    "vivid_hikari_extra.osu": [],
+    "yooh_iceangel_saint.osu": [],
+}
