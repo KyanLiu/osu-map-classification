37824b3ddb32b0773f5f116a9563beb0b9d341f2
Kyan <kyanliu9@gmail.com>
2025-07-30 16:24:45 -0400

    allow api requests for the model

--- server/api/training.py
+++ server/api/training.py
@@ -1,10 +1,18 @@
 from fastapi import APIRouter
 from db import delete_submission
-from train import insertDataById
+from train import OsuModel, insertDataById, shapePredictData
 from pydantic import BaseModel
 from typing import List
 
 router = APIRouter()
+
+osu_model = None
+
+def load_model():
+    global osu_model
+    if osu_model is None:
+        osu_model = OsuModel()
+        osu_model.load()
 
 class TrainData(BaseModel):
     beatmapId: int
@@ -17,10 +25,18 @@ async def train_beatmap(traindata: TrainData):
         insertDataById(traindata.beatmapId, i)
     return {"status": "Training started", "training beatmapId": traindata.beatmapId}
 
-#@router.post('/classify-map{beatmap_id}')
-#async def classify_map_request(beatmap_id: int):
-    #labels = some request function
-    #return { "labels": }
+@router.post('/classify-map/{beatmap_id}')
+async def classify_map_request(beatmap_id: int):
+    global osu_model
+    load_model()
+    data = shapePredictData(beatmap_id)
+    labels = osu_model.predict_class([data])
+    return { "labels": labels}
 
-#@router.post('/find-map{beatmap_id}')
-#async def find_similar_map_request(beatmap_id: int):
+@router.post('/find-map/{beatmap_id}')
+async def find_similar_map_request(beatmap_id: int):
+    global osu_model
+    load_model()
+    data = shapePredictData(beatmap_id)
+    maps = osu_model.find_map([data])
+    return { "maps": maps}
