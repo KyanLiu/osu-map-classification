7689be30c98b497cef5bf89357e412666adf5e5f
Kyan <kyanliu9@gmail.com>
2025-08-26 18:25:05 -0400

    add hover on large screens

--- client/src/components/MapDisplay.tsx
+++ client/src/components/MapDisplay.tsx
@@ -39,6 +39,7 @@ const MapDisplay = ({ detail }: { detail: Map }) => {
   });
   const audioRef = useRef<HTMLAudioElement>(null);
   const [playing, setPlaying] = useState<boolean>(false);
+  const [hover, setHover] = useState<boolean>(false);
 
   const volumeControl = (): void => {
     if (audioRef.current) {
@@ -83,14 +84,14 @@ const MapDisplay = ({ detail }: { detail: Map }) => {
   }, [detail])
 
   return (
-    <div className='w-full bg-gray-800 rounded-xl shadow-lg overflow-hidden'>
+    <div className={`w-full sm:h-70 bg-gray-800 rounded-xl shadow-lg overflow-hidden ${hover ? 'flex flex-col justify-center' : ''}`} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>
       {beatmap?.url && <a href={beatmap.url} target="_blank">
-        {beatmap?.cover && (
-          <div className='h-32'>
+        {beatmap?.cover && !hover && (
+          <div className='h-32' >
             <img src={beatmap.cover} className='w-full h-full object-cover' alt={beatmap?.title} />
           </div>
         )}
-        <div className='p-4 flex justify-between'>
+        <div className='py-4 px-6 flex justify-between items-center' >
           <div>
             {beatmap?.title && <h1 className='text-left text-white text-lg font-bold mb-1'>{beatmap.title}</h1>}
             {beatmap?.version && <h2 className='text-left text-gray-400 text-sm mb-1'>{beatmap.version}</h2>}
@@ -101,6 +102,40 @@ const MapDisplay = ({ detail }: { detail: Map }) => {
               </div>
             )}
           </div>
+          
+          <div className={`m-4 hidden md:flex p-2 grow justify-evenly flex-wrap gap-3 overflow-hidden transition-all duration-500 ease-in-out ${hover ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}>
+            {beatmap?.len && (
+              <div>
+                <p className='text-2xl font-bold text-blue-300'>{beatmap.len}</p>
+                <p className='text-xs text-gray-500 uppercase tracking-wider'>LENGTH</p>
+              </div>
+            )}
+            {beatmap?.ar && (
+              <div>
+                <p className='text-2xl font-bold text-green-300'>{beatmap.ar}</p>
+                <p className='text-xs text-gray-500 uppercase tracking-wider'>AR</p>
+              </div>
+            )}
+            {beatmap?.bpm && (
+              <div>
+                <p className='text-2xl font-bold text-purple-300'>{beatmap.bpm}</p>
+                <p className='text-xs text-gray-500 uppercase tracking-wider'>BPM</p>
+              </div>
+            )}
+            {beatmap?.cs && (
+              <div>
+                <p className='text-2xl font-bold text-yellow-300'>{beatmap.cs}</p>
+                <p className='text-xs text-gray-500 uppercase tracking-wider'>CS</p>
+              </div>
+            )}
+            {beatmap?.drain && (
+              <div>
+                <p className='text-2xl font-bold text-red-300'>{beatmap.drain}</p>
+                <p className='text-xs text-gray-500 uppercase tracking-wider'>HP</p>
+              </div>
+            )}
+          </div>
+          
           <div>
             {beatmap?.audio && (
               <div>
